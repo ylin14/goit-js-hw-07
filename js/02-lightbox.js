9fd0c2028261741d74d3d420e35dc14b3e0b0259
ylin14 <y.linyvyi@gmail.com>
2023-04-23 22:38:18 +0300

    created gallery via simplelightbox lib

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ref = {
+    gallery: document.querySelector('.gallery'),
+  };
+
+//   ref.gallery.addEventListener('click', onGalleryImageClick);
+  
+  function createGalleryMarkup(arr) {
+    return arr
+      .map(
+        ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`,
+      )
+      .join('');
+  };
+  
+  ref.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+  const lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250,
+    captionsData: "alt",
+   });
+
+
