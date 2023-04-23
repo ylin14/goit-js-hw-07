6200f50ea0acf8bbd6905cd37d4fbf293c62da40
ylin14 <y.linyvyi@gmail.com>
2023-04-23 22:10:43 +0300

    created galleri via basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  gallery: document.querySelector('.gallery'),
+};
+
+ref.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+ref.gallery.addEventListener('click', onGalleryImageClick);
+
+function createGalleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+
+function onGalleryImageClick(event) {
+  const evt = event.target;
+
+  const galleryImage = evt.classList.contains('gallery__image');
+  event.preventDefault();
+  if (!galleryImage) return;
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  if (instance.visible()) {
+    document.addEventListener('keydown', event => {
+      if (event.code !== 'Escape') return;
+      instance.close();
+    });
+  }
+}
